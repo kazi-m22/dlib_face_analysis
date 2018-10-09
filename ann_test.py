c06311d486776cb90b290c33a1fd201a9933bc8c
misbah <222mzb@gmail.com>
2018-10-10 00:03:31 +0600

    single data train

--- ann_test.py
+++ ann_test.py
@@ -3,24 +3,29 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
 
 
-data = pd.read_csv('facedata.csv', header = None)
+data = pd.read_csv('features.csv', header = None)
 data = data.values
 
 x = data[:,0:6]
 y = data[:,6]
 y = np.transpose(np.array([y]))
-print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=0.1)
 
 model = Sequential()
 
-model.add(Dense(12, input_dim=6, activation='relu'))
-model.add(Dense(12, activation='relu'))
-model.add(Dense(4,activation='relu'))
-model.add((Dense(1, activation='sigmoid')))
+model.add(Dense(6, input_dim=6, activation='relu'))
 
-opt = Adam(lr = .0001,decay=0.0)
-model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'],)
+model.add(Dense(10, activation='relu'))
+model.add(Dense(10, activation='relu'))
 
-model.fit(x,y,epochs=50, batch_size=5)
+
+model.add(Dense(1, activation='sigmoid'))
+
+opt = Adam(lr = .00001,decay=0.0)
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+model.fit(X_train,y_train,epochs=20,validation_data=(X_test,y_test), batch_size=5)
