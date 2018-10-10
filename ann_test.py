2a0273947857226495c9ecdad00a173c80e7ee94
misbah <222mzb@gmail.com>
2018-10-10 20:03:01 +0600

    single data train and test

--- ann_test.py
+++ ann_test.py
@@ -6,7 +6,7 @@ from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 
 
-data = pd.read_csv('features.csv', header = None)
+data = pd.read_csv('facedata.csv', header = None)
 data = data.values
 
 x = data[:,0:6]
@@ -19,13 +19,13 @@ X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=0.1)
 model = Sequential()
 
 model.add(Dense(10, input_dim=6, activation='relu'))
-model.add(Dense(5, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 opt = Adam(lr = .00001,decay=0.0)
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-model.fit(X_train,y_train,epochs=5,validation_data=(X_test,y_test), batch_size=5)
+model.fit(X_train,y_train,epochs=200,validation_data=(X_test,y_test), batch_size=5)
 
 test_ratim = [26.565,58.57,28.202,59.744,2.935661108,2.375]
 test_me = [29.624,64.983,32.856,69.864,2.818548363,2.355078946]
