0bbcf9282d7d7283bc591a99664844adeee630a2
misbah <222mzb@gmail.com>
2018-10-15 22:48:45 +0600

    standardize

--- ann_test.py
+++ ann_test.py
@@ -4,6 +4,8 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
+from keras import regularizers
+from sklearn.preprocessing import StandardScaler
 
 
 data = pd.read_csv('facedata.csv', header = None)
@@ -16,16 +18,22 @@ y = data[:,6]
 # y = np.expand_dims(y, axis=0)
 X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=0.1)
 
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
 model = Sequential()
 
-model.add(Dense(10, input_dim=6, activation='relu'))
-model.add(Dense(10, activation='relu'))
+model.add(Dense(4, input_dim=6, activation='relu'))
+model.add(Dense(3, activation='relu'))
+# model.add(Dense(5, activation='relu', kernel_regularizer=regularizers.l2(.1)))
+
 model.add(Dense(1, activation='sigmoid'))
 
 opt = Adam(lr = .00001,decay=0.0)
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-model.fit(X_train,y_train,epochs=200,validation_data=(X_test,y_test), batch_size=5)
+model.fit(X_train,y_train,epochs=20,validation_data=(X_test,y_test), batch_size=5)
 
 test_ratim = [26.565,58.57,28.202,59.744,2.935661108,2.375]
 test_me = [29.624,64.983,32.856,69.864,2.818548363,2.355078946]
